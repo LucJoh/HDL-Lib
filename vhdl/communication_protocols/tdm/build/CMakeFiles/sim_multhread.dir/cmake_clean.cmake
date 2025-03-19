6368409f706a50538350873a2195c3ab69ffd09a
lucjoh <lucjoh99hotmail.com>
2025-03-19 20:48:52 +0100

    update gitignore

--- vhdl/communication_protocols/tdm/build/CMakeFiles/sim_multhread.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sim_multhread"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sim_multhread.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- vhdl/communication_protocols/tdm/build/CMakeFiles/write_bitstream.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/write_bitstream"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/write_bitstream.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
